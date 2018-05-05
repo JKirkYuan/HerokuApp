8fe0fa695a27bba0f6bfc7bb6360b301eb361fc2
Kirk Yuan <yuanjt810@gmail.com>
2018-05-05 18:26:07 -0400

    Added all projects and finished section

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const ROOT = './src/site'
-
+const ROOT = './src/site/';
 
 app
     .set('port', (process.env.PORT || 5000))
     .get(['/', '/index.html', '/index'], (req,res) => {
         res.sendFile('index.html', {root: ROOT});
+    })
+
+    .get(['/Legacy_Site/index.html'], (req, res) => {
+        res.sendFile('./Legacy_Site/index.html', {root: ROOT});
     })
 
     .use(express.static(ROOT))
